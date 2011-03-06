1070c00b10c49cf58079746349404c5dfd9eca11
Adam Carlile <adam@benchmedia.co.uk>
2011-03-05 22:25:57 -0800

    changed spec to reflect changes in model methods

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -11,11 +11,11 @@ describe Bookmark do
   end
   
   it "Should return the title tag from the url" do
-    @bookmark.document_title.should == 'BBC - Homepage'
+    @bookmark.title.should == 'BBC - Homepage'
   end
   
   it "Should return the meta data from the url" do
-    @bookmark.document_description.should == 'Breaking news, sport, TV, radio and a whole lot more. The BBC informs, educates and entertains - wherever you are, whatever your age.'
+    @bookmark.description.should == 'Breaking news, sport, TV, radio and a whole lot more. The BBC informs, educates and entertains - wherever you are, whatever your age.'
   end
   
 end
